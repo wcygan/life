9002f949c9c760fb30fde93156e0cf02232f40ba
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-12-30 19:37:56 -0600

    tweaked some of the constants

--- src/main/java/io/wcygan/board/Constants.java
+++ src/main/java/io/wcygan/board/Constants.java
@@ -2,9 +2,9 @@ package io.wcygan.board;
 
 public class Constants {
     public static final Integer BOARD_SIDE_LENGTH = 2 << 9;
-    public static final Integer TILES_PER_SIDE = 2 << 4;
+    public static final Integer TILES_PER_SIDE = 2 << 5;
     public static final Integer TILE_LENGTH = BOARD_SIDE_LENGTH / TILES_PER_SIDE;
     public static final Integer CUSTOMIZATION_PANEL_LENGTH = 150;
     public static final Integer MAXIMUM_ITERATIONS = 10000;
-    public static final Long MILLISECONDS_TO_SLEEP = 100L;
+    public static final Long MILLISECONDS_TO_SLEEP = 80L;
 }
